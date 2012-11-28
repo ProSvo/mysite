1ce534dfe71734702340be5b918742700b4e5703
Roman Humennyy <roman.humenny@gmail.com>
2012-11-28 22:39:01 +0200

    change

--- mysite/views.py
+++ mysite/views.py
@@ -21,4 +21,3 @@ def hours_ahead(request, offset):
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
 	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
 	return HttpResponse(html)
-
